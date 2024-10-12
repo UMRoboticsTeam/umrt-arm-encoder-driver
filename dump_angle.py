ce68ef91fd440c28ce4bf0daf3fedd32b493062a
Noah Reeder <noahreederatc@gmail.com>
2024-10-12 16:48:10 -0500

    Added program to dump the 2nd and 3rd bytes of the suspected angle message. This is the portion of the message which I suspected contained the angle value. Based on this test I do not think it does.

--- dump_angle.py
+++ dump_angle.py
@@ -48,7 +48,7 @@ def dump_delta():
                 if msg is not None and len(msg.data) == 8:
                     if msg.data[0] == 0x55 and msg.data[1] == 0x55:
                         output_msg = []
-                        for i in range(8):
+                        for i in range(2, 4):
                             output_msg.append(msg.data[i] - last_msg[i])
                         last_msg = msg.data
                         print(output_msg)
@@ -56,6 +56,32 @@ def dump_delta():
         except KeyboardInterrupt:
             pass  # exit normally
 
+def dump_just_angle():
+    """Receives all messages and prints the angle portion of each message to the console until Ctrl+C is pressed."""
+
+    # this uses the default configuration (for example from environment variables, or a
+    # config file) see https://python-can.readthedocs.io/en/stable/configuration.html
+    with can.Bus(interface='slcan', channel='COM5', bitrate=250000) as bus:
+        # set to read-only, only supported on some interfaces
+        try:
+            bus.state = BusState.PASSIVE
+        except NotImplementedError:
+            pass
+
+        try:
+            last_msg = bytearray([0x55, 0x55, 0x3c, 0x00, 0x00, 0x00, 0xf2, 0xff])
+            while True:
+                msg = bus.recv(1)
+                if msg is not None and len(msg.data) == 8:
+                    if msg.data[0] == 0x55 and msg.data[1] == 0x55:
+                        output_msg = []
+                        output_msg.append(msg.data[2])
+                        output_msg.append(msg.data[3])
+                        print(output_msg)
+
+        except KeyboardInterrupt:
+            pass  # exit normally
+
 
 if __name__ == "__main__":
-    dump_delta()
+    dump_just_angle()
