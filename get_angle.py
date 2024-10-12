ceceb96eaa42e07b5c74a7a57cc68405e712f6f2
Noah Reeder <noahreederatc@gmail.com>
2024-10-12 15:57:49 -0500

    Attempted send_read_request implementation

--- get_angle.py
+++ get_angle.py
@@ -9,8 +9,8 @@ import can
 def send_read_request():
     with can.Bus(interface='slcan', channel=COM_PORT, bitrate=250000) as bus:
         msg = can.Message(arbitration_id=0x50,
-                          data=[0,0],
-                          is_extended_id=True)
+                          data=[0x55, 0x55, 0xaa, 0xbb, 0xcc, 0xdd, 0xee, 0xff],
+                          is_extended_id=False)
 
         try:
             bus.send(msg)
