86e88bdc247ad93cacce06ae9e5ce1c849c3faeb
Toni Odujinrin <todujinrin@gmail.com>
2024-12-28 17:45:23 -0600

    finished some read handlers

--- cpp/encoder_interface.cpp
+++ cpp/encoder_interface.cpp
@@ -34,15 +34,67 @@ CANAPI_Return_t Interface::initialize_channel(){
 	return ret_val; 
 }
 
+
+
+
 void Interface::begin_read_loop(const char type, int timeout){
 	int start_time = std::time(nullptr); 
 	while(timeout?exception_flag&start_time-std::time(nullptr)<=timeout:exception_flag){
 		if(ret_val = mySerialCAN.ReadMessage(message,timeout?timeout:CANREAD_INFINITE) == CSerialCAN::NoError){
-				
+				if(message.dlc == 8){
+					switch(type){
+						case 'a':
+							handle_angle(message.data); 
+							break; 
+						case 't':
+							handle_temp(message.data); 
+							break; 
+						case 'e':
+							handle_all(message.data); 
+							break; 
+						case 'd':
+							handle_delta(message.data);
+							break; 
+						default:
+							std::cout<<"[x] unknown read operation "<<type<<" exiting..."<<"\n"; 
+							exception_flag = 0;
+					}
+				}
 		}
 	}
 }; 
 
+void Interface::handle_all(uint8_t* message_data){
+	std::cout<<"message id: "<<message.id<<"\n"<<"message data count: "<<message.dlc<<"\n"
+	<<"message time stamp(s): "<<message.timestamp.tv_sec<<"\n"<<"message data: "; 
+	for (size_t i{}; i < message.dlc; i++){
+		printf("The value in hexadecimal is: 0x%02X", message.data[i]);
+	};
+	std::cout<<std::endl; 
+}; 
+
+void Interface::handle_angle(uint8_t* message_data){
+	if(message_data[0] == 0x55 && message_data[1] == 0x55){
+		uint16_t angle_register = message_data[3]<<8|message_data[2]; 
+		double angle = angle_register * 360/32768; 
+		uint16_t angular_velocity_register = message_data[5]<<8|message_data[4]; 
+		double angular_velocity = angular_velocity_register * ((360/32768)/0.1); 
+		uint16_t number_of_rotations = message_data[8]<<8|message_data[7]; 
+		printf("angle register(0x%02X) = %0.4f ° \n angualr velocity register(0x%02X) = %0.4f °/s \n number of roatations = %d",angle_register,angle, angular_velocity_register, angular_velocity, number_of_rotations); 
+	}
+}; 
+
+void Interface::handle_temp(uint8_t* message_data){
+	if(message_data[0] == 0x55 && message_data[1] == 0x56){
+		uint16_t temperature_register = message_data[3]<<8|message_data[2]; 
+		double temperature = temperature_register/100; 
+		printf("temperature register(0x%02X) = %0.4f °C",temperature_register, temperature); 
+	}
+}; 
+
+// void Interface::handle_delta(uint8_t* message_data){
+
+// }; 
 
 
 CANAPI_Return_t Interface::teardown_channel(){ 
