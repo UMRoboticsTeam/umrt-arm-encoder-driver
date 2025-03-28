f593777732be5ff77150bad3c61c2cdceeb3dadf
Noah Reeder <noahreederatc@gmail.com>
2025-03-27 20:16:48 -0500

    Ran an autoformat

--- include/arm-encoder-driver/encoder_interface.h
+++ include/arm-encoder-driver/encoder_interface.h
@@ -13,18 +13,17 @@
 #include <unistd.h>
 
 
-#ifdef __linux__ 
-    typedef uint8_t __u8;
-#endif 
+#ifdef __linux__
+typedef uint8_t __u8;
+#endif
 
 
 class EncoderInterface {
 private:
-    
     struct ifreq ifr;
     struct sockaddr_can addr;
     int can_socket;
-    uint8_t* previous_data; 
+    uint8_t* previous_data;
 
 
     void handle_angle(uint8_t* message_data, uint32_t can_id);
@@ -35,7 +34,7 @@ private:
 
 public:
     EncoderInterface() = default;
-    ~EncoderInterface(); 
+    ~EncoderInterface();
     int initialize_channel(const char* can_interface);
     void begin_read_loop();
     boost::signals2::signal<void(uint32_t can_id, double angle, double angular_velocity, uint16_t number_of_rotations)> angle_signal;
--- src/encoder_interface.cpp
+++ src/encoder_interface.cpp
@@ -1,9 +1,9 @@
 #include "encoder_interface.h"
 
-EncoderInterface::~EncoderInterface(){
-    if(can_socket >= 0){
-        close(can_socket); 
-        can_socket = -1 ; 
+EncoderInterface::~EncoderInterface() {
+    if (can_socket >= 0) {
+        close(can_socket);
+        can_socket = -1;
     }
 };
 
@@ -20,7 +20,7 @@ int EncoderInterface::initialize_channel(const char* can_interface) {
     strcpy(ifr.ifr_name, can_interface);
     BOOST_LOG_TRIVIAL(info) << "[+] fetching interface";
     if (ioctl(can_socket, SIOGIFINDEX, &ifr) < 0) {
-        BOOST_LOG_TRIVIAL(error) << "[x] could not find can interface: "<<can_interface;
+        BOOST_LOG_TRIVIAL(error) << "[x] could not find can interface: " << can_interface;
         close(can_socket);
         return -1;
     }
@@ -42,12 +42,12 @@ void EncoderInterface::begin_read_loop() {
     while (true) {
         can_frame message;
         ssize_t nbytes = read(can_socket, &message, sizeof(can_frame));
-        if (nbytes>0 && message.len == 8) {
+        if (nbytes > 0 && message.len == 8) {
             handle_angle(message.data, message.can_id);
-            handle_temp(message.data,message.can_id);
+            handle_temp(message.data, message.can_id);
             handle_all(message);
             handle_delta(message.data, previous_data);
-            previous_data = message.data; 
+            previous_data = message.data;
         } else {
             BOOST_LOG_TRIVIAL(error) << "[x] invalid data length " << message.len;
         }
@@ -62,7 +62,7 @@ void EncoderInterface::handle_angle(uint8_t* message_data, uint32_t can_id) {
     if (message_data[0] == 0x55 && message_data[1] == 0x55) {
         uint16_t angle_register_value = uint16_t(message_data[3]) << 8 | message_data[2];
         double angle = angle_register_value * 360 / 32768;
-        uint16_t angular_velocity_register_value = uint16_t(message_data[5])<<8 | message_data[4];
+        uint16_t angular_velocity_register_value = uint16_t(message_data[5]) << 8 | message_data[4];
         double angular_velocity = angular_velocity_register_value * ((360 / 32768) / 0.1);
         uint16_t number_of_rotations = uint16_t(message_data[7]) << 8 | message_data[6];
         angle_signal(can_id, angle, angular_velocity, number_of_rotations);
@@ -77,21 +77,17 @@ void EncoderInterface::handle_temp(uint8_t* message_data, uint32_t can_id) {
     }
 };
 
-void EncoderInterface::handle_delta(uint8_t* message_data, uint8_t* previous_data){
-    if (message_data[1] == previous_data[1]){  //if both messages are of same type i.e both temperature messages or both angle messages then find difference
-        if(message_data[1] == 0x56){
-            BOOST_LOG_TRIVIAL(debug)<<"previous message data : "; 
-          for(int i=0;  i < 8; i++){
-            BOOST_LOG_TRIVIAL(debug)<<previous_data[i]<<" "; 
-          }
-            BOOST_LOG_TRIVIAL(debug)<<"current message data : "; 
-          for(int i=0;  i < 8; i++){
-            BOOST_LOG_TRIVIAL(debug)<<message_data[i]<<" "; 
-          }
-          
-
+void EncoderInterface::handle_delta(uint8_t* message_data, uint8_t* previous_data) {
+    if (message_data[1] == previous_data[1]) { //if both messages are of same type i.e both temperature messages or both angle messages then find difference
+        if (message_data[1] == 0x56) {
+            BOOST_LOG_TRIVIAL(debug) << "previous message data : ";
+            for (int i = 0; i < 8; i++) {
+                BOOST_LOG_TRIVIAL(debug) << previous_data[i] << " ";
+            }
+            BOOST_LOG_TRIVIAL(debug) << "current message data : ";
+            for (int i = 0; i < 8; i++) {
+                BOOST_LOG_TRIVIAL(debug) << message_data[i] << " ";
+            }
         }
     }
 };
-
-
